7fc2078767b87da14c19e5f001ceea117c20ebc3
Martin Maechler <maechler@r-project.org>
2019-04-02 07:56:12 +0200

    update (to newer 'lmerTest'),  plus extra remarks by MM

--- R/lmerTest_v82i13.R
+++ R/lmerTest_v82i13.R
@@ -38,7 +38,7 @@ simCompare <- function(mod, nsim) {
 
     an.sat <- anova(mod2)
     ## if (require("pbkrtest"))
-    an.kr <- anova(mod2, ddf = "kenw")
+    an.kr <- anova(mod2, ddf = "Kenward-Roger")
     Mod_no.treat <- lmer(ysim ~ 1 + (1|Block), data = PBIB)
     w_no.treat <- anova(mod2, Mod_no.treat)
 
@@ -54,28 +54,51 @@ summary(fm2P.)
 
 ## Model corresponding to the null hypothesis of no treatment effect.
 fm2PBIB <- lmerTest::lmer(response ~ 1 + (1|Block), PBIB)
-summary(fm2PBIB)
+summary(fm2PBIB) # with P-values
 
 set.seed(998)
 nsim <- 1000
+## takes some minutes .. and produces 1000+ messages
+if(FALSE)
 ressim <- simCompare(fm2PBIB, nsim)
+## ==> rather cache the result for MEMo lecture:
+sfile <- "~/Vorl/MEMo/examples_R/lmerTest_sim_m2PBIB.rds"
+if(file.exists(sfile)) {
+    ressim <- readRDS(sfile)
+} else {
+    ressim <- simCompare(fm2PBIB, nsim) ## takes some minutes .. and produces 1000+ messages
+    "refitting model(s) with ML (instead of REML)"
+    ## in addition, (2019-03):
+        ## Warning messages:
+        ## 1: In optwrap(optimizer, devfun, x@theta, lower = x@lower, calc.derivs = TRUE,  :
+        ##   convergence code 3 from bobyqa: bobyqa -- a trust region step failed to reduce q
+        ## 2: In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
+        ##   Model failed to converge with max|grad| = 0.0217152 (tol = 0.002, component 1)
+    saveRDS(ressim, sfile)
+}
 
-pvals <- ressim$res_no_treat[, "pvalSat"]
+pvals     <- ressim$res_no_treat[, "pvalSat"]
 pvals.lrt <- ressim$res_no_treat[, "pvalLRT"]
-pvals.kr <- ressim$res_no_treat[, "pvalKenw"]
+pvals.kr  <- ressim$res_no_treat[, "pvalKenw"]
 
-n <- nsim
-empLRT  <- empSAT <- empKR <- rep(NA_real_, n)
+empLRT <- empSAT <- empKR <- rep(NA_real_, nsim)
 alphas <- seq(0.001, 1, by = 0.001)
 for (i in 1:length(alphas)) {
-  empSAT[i] <- sum(pvals < alphas[i]) / nsim
-  empKR[i] <- sum(pvals.kr < alphas[i]) / nsim
+  empSAT[i] <- sum(pvals     < alphas[i]) / nsim
+  empKR [i] <- sum(pvals.kr  < alphas[i]) / nsim
   empLRT[i] <- sum(pvals.lrt < alphas[i]) / nsim
 }
 
 dd <- data.frame(empSAT, empKR, empLRT, alphas)
+str(dd)
+head(dd)
+## MM: simple traditional graphics plot
+matplot(dd$alphas, dd[,1:3], type="l", lty=1, lwd=2, asp=1,
+        xlab = quote("nominal" ~ alpha), ylab = "empirical P values")
+abline(0,1, col="gray50", lty=2); grid()
+legend("topleft", c("Satterthwaite", "Kenward-Roger", "LRT"), lty=1, lwd=2, col=1:3)
 
-## Melt data for satterthwaite.
+## Melt data for ggplot()ing :
 ddplot <- melt(dd, measure.vars = c("empSAT", "empKR", "empLRT"),
                variable_name = "test",
                value.name = "observed test level")
@@ -101,6 +124,10 @@ library("lme4")
 tv4 <- lme4::lmer(Sharpnessofmovement ~ TVset * Picture +
                (1 | Assessor) + (1 | Assessor:TVset) + (1 | Assessor:Picture),
                data = TVbo)
+## MM:
+summary(tv4) # shows correlations of fixed effects --> symbolically:
+print(summary(tv4), symbolic.cor=TRUE)
+
 anova(tv4)
 
 library("lmerTest")
@@ -108,53 +135,62 @@ tv <- lmerTest::lmer(Sharpnessofmovement ~ TVset * Picture +
                        (1 | Assessor) + (1 | Assessor:TVset) + (1 | Assessor:Picture),
                      data = TVbo)
 anova(tv)
-anova(tv, type = 2, ddf = "Kenward-Roger") # here exactly the same
+anova(tv, type = 2, ddf = "Kenward-Roger") # here "exactly" the same (ddf are more accurat)
 
 ##################################################################
 
 ##################################################################
 ## Example in Section 8.3
 m.carrots <- lmer(Preference ~ sens1 + sens2 +
-                      (1 + sens1 + sens2|Consumer) + (1|product),
+                      (1 + sens1 + sens2|Consumer) + (1|Product),
                   data = carrots)
 summary(m.carrots)
 ##################################################################
 
 ##################################################################
 ## Example in Section 8.4
+## -- here, the very full model
 tv <- lmer(Sharpnessofmovement ~ TVset * Picture +
                (1 | Assessor:TVset) + (1 | Assessor:Picture) +
                (1 | Assessor:Picture:TVset) + (1 | Repeat) + (1 | Repeat:Picture) +
                (1 | Repeat:TVset) + (1 | Repeat:TVset:Picture) +
                (1 | Assessor), data = TVbo)
-st <- step(tv)
+##-> message  "boundary (singular) fit:"
+st <- step(tv) # more singular fit messages
 plot(st, effs = c("Picture", "TVset"))
 
-## Wrap the results to xtable from example in Section 8.4
-## Wrap the random part.
+st0 <- st
+" ==== FIXME: 'st' is different!"
+
+## Wrap the results to xtable from example in Section 8.4  -- page 13
 library("xtable")
-st$rand.table[, 4] <- format.pval(st$rand.table[, 4], digits = 3, eps = 1e-3)
-colnames(st$rand.table) <- c("$\\chi^2$", "$\\text{Chi.DF}$" ,
-                             "$\\text{elim.num}$", "$p\\text{-value}$")
-rand.table_tv <- xtable(st$rand.table, align = "lcccc",
-                        display = c("s", "f", "d", "d", "s"),
+## Wrap the random part.________FIXME now has 7 columns !!
+colnames(st0$random)
+##  "Eliminated" "npar"  "logLik"  "AIC"  "LRT"  "Df" "Pr(>Chisq)"
+st$random[, "Pr(>Chisq)"] <- format.pval(st0$random[, "Pr(>Chisq)"], digits = 3, eps = 1e-3)
+st$random[, "Eliminated"] <- local({e <- st0$random[, "Eliminated"]; e[e == 0] <- "kept"; e })
+names(st$random) <- c("$\\text{elim.num}$", "npar", "logLik", "AIC", "$\\chi^2$", "$\\text{Chi.DF}$" ,
+                                                        "$p\\text{-value}$")
+rand.table_tv <- xtable(st$random,
+                        align = "lccccccc",
+                        display = c("s", "f", "d","d","d", "d", "d", "s"),
                         label = "tab:rand")
 
 caption(rand.table_tv) <- "Likelihood ratio tests for the
 random-effects and their order of elimination representing Step 1 of
 the automated analysis for the TVbo data for attribute Sharpnessofmovement"
 
-## Wrap the fixed part.
 print(rand.table_tv, caption.placement = "top", table.placement = "H",
       sanitize.text.function = function(x) x, size = "\\small")
 
-st$anova.table[, 7] <- format.pval(st$anova.table[, 7], digits = 3, eps = 1e-3)
-colnames(st$anova.table) <-
-  c("Sum Sq", "Mean Sq", "NumDF", "DenDF", "$\\text{F.value}$",
-    "$\\text{elim.num}$", "Pr(>F)")
+## Wrap the fixed part. (s / anova.table / fixed /):
 
-anova.table_tv <- xtable(st$anova.table, align = "lccccccc",
-                         display = c("s", "f", "f", "s", "f", "f", "s", "s"),
+st$fixed[, "Pr(>F)"] <- format.pval(st0$fixed[,  "Pr(>F)"], digits = 3, eps = 1e-3)
+colnames(st$fixed) <-
+  c("$\\text{elim.num}$", "Sum Sq", "Mean Sq", "NumDF", "DenDF", "$\\text{F.value}$", "Pr(>F)")
+
+anova.table_tv <- xtable(st$fixed, align = "lccccccc",
+                         display = c("s", "s", "f", "f", "s", "f", "f", "s"),
                          label = "tab:fixed")
 caption(anova.table_tv) <- "$F$-tests for the fixed-effects and their
 order of elimination representing Step 3 of the automated analysis for the TVbo data for attribute Sharpnessofmovement"
